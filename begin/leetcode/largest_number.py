fd236bdf7d834a1e3355f9aedda543f2856368c9
Allan L <luciocorreia.allan@gmail.com>
2020-09-25 09:48:08 -0300

    Create largest_number.py

---
+++ begin/leetcode/largest_number.py
@@ -0,0 +1,34 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        def comparator(a, b): 
+            ab = str(a) + str(b) 
+            ba = str(b) + str(a) 
+            return ((int(ba) > int(ab)) - (int(ba) < int(ab))) 
+      
+        def myCompare(mycmp): 
+
+            # Convert a cmp= function into a key= function 
+            class K(object): 
+                def __init__(self, obj, *args): 
+                    self.obj = obj 
+                def __lt__(self, other): 
+                    return mycmp(self.obj, other.obj) < 0
+                def __gt__(self, other): 
+                    return mycmp(self.obj, other.obj) > 0
+                def __eq__(self, other): 
+                    return mycmp(self.obj, other.obj) == 0
+                def __le__(self, other): 
+                    return mycmp(self.obj, other.obj) <= 0
+                def __ge__(self, other): 
+                    return mycmp(self.obj, other.obj) >= 0
+                def __ne__(self, other): 
+                    return mycmp(self.obj, other.obj) != 0
+            return K
+    
+    
+        sorted_array = sorted(nums, key=myCompare(comparator)) 
+        number = "".join([str(i) for i in sorted_array]) 
+        if(number.replace("0","") == ""): 
+            return "0"
+        return number 
